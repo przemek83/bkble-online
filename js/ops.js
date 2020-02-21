fada1ce139946e914f9d2937e31d2b3e8866a768
przemek83 <4788832+przemek83@users.noreply.github.com>
2020-02-21 09:17:18 +0100

    Initial import
    Initial import of 7 years old page.

---
+++ js/ops.js
@@ -0,0 +1,208 @@
+function Knight(knightAsString) 
+{
+    var afterSplit = knightAsString.trim().split(/\t/g);
+//    var nameSplitted = afterSplit[0].trim().replace(/ \[.*\].*/g, '').split(' ');
+//    this.name = nameSplitted[nameSplitted.length-1];
+    this.name = afterSplit[0].replace(/\[.*\]/g, '');
+    this.order = afterSplit[0].match(/\[.*\]/g);
+    if(this.order === null)
+        this.order = "";
+    var fromIndex;
+    if(afterSplit.length === 7) 
+        fromIndex = 2;
+    else 
+        fromIndex = 1; 
+    this.level = afterSplit[fromIndex].trim();
+    this.lootText = String(afterSplit[fromIndex+1]);
+    this.loot = Number(afterSplit[fromIndex+1].replace(/\./g,'').trim());
+    this.fights = Number(afterSplit[fromIndex+2].replace(/\./g,'').trim());
+    this.win = Number(afterSplit[fromIndex+3].replace(/\./g,'').trim());
+    this.place = Number(afterSplit[fromIndex+4].match(/\n.*/g))-1;
+    this.loose = Number(afterSplit[fromIndex+4].replace(/\./g,'').replace(/\n.*/g, '').trim());
+    this.lootFromCheckPoint = 0;
+    this.lootDiff = 0;
+    this.ignore = false;
+    
+    this.print = function()
+    {
+        return this.lootDiff + " " + this.place + " " + this.name + " " + this.order + " " + this.level + " " + this.lootText + "\n";
+    };
+    
+    this.createRow = function(row)
+    {
+            var tbodyString = "";
+            tbodyString += '<td>';
+            tbodyString += this.lootDiff;
+            tbodyString += '</td><td>';
+            tbodyString += this.place;
+            tbodyString += '</td><td>';
+            tbodyString += this.name;
+            tbodyString += '</td><td>';
+            tbodyString += this.order;
+            tbodyString += '</td><td>';
+            tbodyString += this.level;
+            tbodyString += '</td><td>';
+            tbodyString += this.lootText;
+            tbodyString += '</td><td>';
+            tbodyString += "<center><button class=\"btnIgnore\" name=\"button\" id=\"button\" onclick=\"ignoreKnight(" + row +  ");\">x</button></center>";
+            tbodyString += '</td>';
+            return tbodyString;
+    };
+    
+    this.calculateLootDiff = function()
+    {
+        this.lootDiff =  this.loot - this.lootFromCheckPoint;
+    };
+};
+
+var knightsArray = new Array();
+
+function dataPasted(textarea)
+{
+    var textToParse = textarea.value; 
+    var ranks = document.getElementById("rankTextArea");
+    var console = document.getElementById("console");
+    
+    var splitted = textToParse.split(RegExp("\t\n"));
+    if(console !== null)
+        console.value = console.value + "splitted has " + splitted.length + " lines\n";
+    //ranks.value = splitted[3];
+    
+    if(splitted.length !== 103 && splitted.length !== 107)
+    {
+         if(console !== null)
+            console.value = console.value +"error of split\n";
+         textarea.value = "";
+         return;
+    }
+    
+    var slip = 0;
+    if(splitted.length === 103)
+        slip = 3;
+    else
+        slip = 7;
+    
+    var lastPlace = 0;
+    for (var i=0; i<100; i++)
+    {
+        //console.value = console.value + "creating knight from string " + splitted[slip+i] + "\n";
+        var knight = new Knight(splitted[slip+i]);
+        
+        if(i === 98)
+            lastPlace = knight.place;
+        
+        if(i === 99)
+            knight.place = lastPlace+1;
+        
+        //console.value = console.value + "created knight " + knight.print();
+        if(false === checkIfKnightInArrayAndUpdate(knight))
+        {
+                knightsArray[knightsArray.length] = knight;
+        }
+        //toPrint +=  knight.print();
+    }
+    
+    knightsArray.sort(compare);
+    //ranks.value = toPrint;
+    //ranks.value = splitted;
+    
+    createTable();
+    //printKnights(ranks);
+    
+    textarea.value = "";
+};
+
+function compare(a,b) 
+{
+    if (a.lootDiff < b.lootDiff)
+    {
+        return 1;
+    }
+    if (a.lootDiff > b.lootDiff)
+    {
+        return -1;
+    }
+    return 0;
+}
+
+function createTable()
+{
+    var theader = '<table id=\"rounded-corner\" >';
+    var tbody = '<tbody>';
+    tbody += "<thead><tr>";
+    tbody += "<th scope=\"col\">Increase</th>";
+    tbody += "<th scope=\"col\">Place</th>";
+    tbody += "<th scope=\"col\">Knight</th>";
+    tbody += "<th scope=\"col\">Order</th>";
+    tbody += "<th scope=\"col\">Level</th>";
+    tbody += "<th scope=\"col\">Loot</th>";
+    tbody += "<th scope=\"col\">Ignore</th>";
+    tbody += "</tr>";
+    tbody += "</thead>";
+    for( var i = 0;  i < knightsArray.length; i++)
+    {
+        if(knightsArray[i].ignore === true)
+            continue;
+        
+        tbody += '<tr>';
+        tbody +=  knightsArray[i].createRow(i);
+        tbody += '</tr>\n';
+    }
+    tbody += "</tbody>";
+    var tfooter = '</table>';
+    document.getElementById('wrapper').innerHTML = theader + tbody + tfooter;
+}
+
+function printKnights(ranks)
+{
+    var toPrint = "";
+     for (i=0; i<knightsArray.length; i++)
+    {
+        toPrint +=  knightsArray[i].print();
+    }
+    ranks.value = toPrint;
+};
+
+function checkIfKnightInArrayAndUpdate(knight)
+{
+     for (i=0; i<knightsArray.length; i++)
+    {
+        if(knightsArray[i].name === knight.name)
+        {
+            knight.ignore = knightsArray[i].ignore;
+            knight.lootFromCheckPoint = knightsArray[i].lootFromCheckPoint;
+            knight.calculateLootDiff();
+            knightsArray[i] = knight;
+            return true;
+        }
+    }
+    knight.lootFromCheckPoint = knight.loot;
+    return false;
+};
+
+
+function saveCheckpoint()
+{
+    var console = document.getElementById("console");
+    if(console !== null)
+        console.value = console.value + "Checkpoint saved\n";
+    for (i=0; i<knightsArray.length; i++)
+    {
+        knightsArray[i].lootFromCheckPoint =  knightsArray[i].loot;
+        knightsArray[i].lootDiff = 0;
+    }
+    
+    createTable();
+    //printKnights(document.getElementById("rankTextArea"));
+};
+
+function setbg(color)
+{
+    document.getElementById("styled").style.background=color;
+};
+
+function ignoreKnight(number)
+{
+     knightsArray[number].ignore = true;
+     createTable();
+};
